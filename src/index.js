dd9d327ca747784b4089585e94a864753b543324
vyom1102 <95407663+vyom1102@users.noreply.github.com>
2022-06-04 01:05:14 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './app';
+import 'tachyons';
+
+
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App/>);
